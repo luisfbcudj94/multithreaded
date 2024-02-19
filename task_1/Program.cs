8e1c7600a5e3d7a57531801920b658142b1288c9
LuisPerez <alejandroabril98@gmail.com>
2024-02-19 14:52:22 -0500

    feat: Search string in file

---
+++ task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+// paths separated by commas
+string filePathsString = "./files/file1.txt,./files/file2.txt,./files/file3.txt,./files/file4.txt";
+
+string search = "*BGAA*";
+
+string[] filePaths = filePathsString.Split(',');
+List<Task<bool>> tasks = new();
+
+foreach (var filePath in filePaths)
+{
+    if (!string.IsNullOrEmpty(filePath))
+        tasks.Add(Task.Run(() => SearchStringInFile(filePath, search)));
+}
+
+await Task.WhenAll(tasks);
+
+for (int i = 0; i < tasks.Count; i++)
+{
+    Console.WriteLine($"File {filePaths[i]}: {tasks[i].Result}");
+}
+
+static bool SearchStringInFile(string filePath, string searchString)
+{
+    try
+    {
+
+        var content = File.ReadAllText(filePath);
+
+        return content.Contains(searchString);
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error reading file {filePath}: {ex.Message}");
+        return false;
+    }
+}
