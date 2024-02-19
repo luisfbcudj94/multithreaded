2c3a22bf240ff82277ca5792a782e48a75b90054
LuisPerez <alejandroabril98@gmail.com>
2024-02-19 18:00:03 -0500

    feat: bucketize

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,39 +1,59 @@
 ﻿using System.Runtime.CompilerServices;
 
-// paths separated by commas
-string filePathsString = "./files/file1.txt,./files/file2.txt,./files/file3.txt,./files/file4.txt";
+// Generate paths
+string filePathsString = "";
+for (int i = 0; i < 100; i++)
+{
+    filePathsString += $"./files/file{i}.txt,";
+}
 
 string search = "*BGAA*";
 
+int maxFilesPerBucket  = Environment.ProcessorCount;
 string[] filePaths = filePathsString.Split(',');
-List<Task<bool>> tasks = new();
 
-foreach (var filePath in filePaths)
+var buckets = BucketizeFilePaths(filePaths, maxFilesPerBucket);
+
+foreach (var bucket in buckets)
 {
-    if (!string.IsNullOrEmpty(filePath))
-        tasks.Add(Task.Run(() => SearchStringInFile(filePath, search)));
+    List<Task<bool>> tasks = new();
+    foreach (var filePath in bucket)
+    {
+        if (!string.IsNullOrEmpty(filePath))
+            tasks.Add(Task.Run(() => SearchStringInFileAsync(filePath, search)));
+    }
+
+    await Task.WhenAll(tasks);
+
+    for (int i = 0; i < tasks.Count; i++)
+    {
+        var bucketList = bucket.ToList();
+        Console.WriteLine($"File path: {bucketList[i]} - Files contain string: {tasks[i].Result}");
+    }
+
+    Console.WriteLine($"NEXT BUCKET");
 }
 
-await Task.WhenAll(tasks);
-
-for (int i = 0; i < tasks.Count; i++)
-{
-    Console.WriteLine($"File {filePaths[i]}: {tasks[i].Result}");
-}
-
-static bool SearchStringInFile(string filePath, string searchString)
+static async Task<bool> SearchStringInFileAsync(string filePath, string searchString)
 {
     try
     {
-
-        var content = File.ReadAllText(filePath);
-
+        var content = await File.ReadAllTextAsync(filePath);
         return content.Contains(searchString);
-
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"Error reading file {filePath}: {ex.Message}");
+        // Console.WriteLine($"Error reading file {filePath}: {ex.Message}");
         return false;
+    }
+}
+
+static IEnumerable<IEnumerable<string>> BucketizeFilePaths(string[] filePaths, int maxFilesPerBucket)
+{
+    int numBuckets = (int)Math.Ceiling((double)filePaths.Length / maxFilesPerBucket);
+
+    for (int i = 0; i < numBuckets; i++)
+    {
+        yield return filePaths.Skip(i * maxFilesPerBucket).Take(maxFilesPerBucket);
     }
 }
--- task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
+++ task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b945524e80ad8ebdb9369df87f692aa68c74ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee05bbc41f189db91c219482169b9ec489f73a4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
